8b796a9b6c49c48d2137d5ea9fc299d115c9aed8
mpak <starovoitov.anton@gmail.com>
2019-02-04 15:46:37 +0500

    добавлена отрисовка графа

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 import datetime
+from plotly import tools
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+tools.set_credentials_file(username='starovoitov.anton', api_key='eWEiOWPqQYFUrS0xiNmo')
 
 
 TOKEN = "525005235250052352500523d9520f2aaa55250525005230ba586e141c5ed5a99a33da2"
@@ -27,24 +32,34 @@ def count_posts(json):
 def get_timestamp_tuple(date):
     week = 7  # days
     timestamp_list = []
+    dates_list = []
     for day in range(week):
         temp_day = date - datetime.timedelta(days=1)
+        dates_list.append(temp_day)
         timestamp_list.append(temp_day.timestamp())
-    return timestamp_list
+        date = temp_day
+    return timestamp_list, dates_list
+
+
+def create_graph(dates, counts):
+    trace0 = go.Scatter(
+        y=[date for date in dates],
+        x=[count for count in counts]
+    )
+    data = [trace0, ]
+
+    py.plot(data, filename='basic-line', auto_open=False)
 
 
 def main():
     date = datetime.datetime.now()
-    dates_timestamps = get_timestamp_tuple(date)
-    all_posts = []
-    total_posts = []
+    dates_timestamps, dates_list = get_timestamp_tuple(date)
+    all_posts_counts = []
     for timestamp in dates_timestamps:
         data_json = get_posts(timestamp)
-        all_posts.append(data_json)
         current_count = count_posts(data_json)
-        total_posts.append(current_count)
-    print(sum(total_posts))
-
+        all_posts_counts.append(current_count)
+    create_graph(all_posts_counts, dates_list)
 
 if __name__ == '__main__':
     main()
